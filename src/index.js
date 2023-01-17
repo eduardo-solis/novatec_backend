9c9307771973ca8e403bafbc444b0b78009e2bc6
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-17 14:37:17 -0600

    Instalando la dependencia de mysql2 y realizando una prueba de conexion con la base de datos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { pool } from "./db.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hola mundo");
+});
+
+
+// Consulta para verificar la conexión del proyecto con la base de datos
+app.get("/ping", async (req, res) => {
+    const [result] = await pool.query('select "PONG" as result');
+    res.json(result[0]);
 });
 
 app.listen(3000);
