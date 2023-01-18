a3926ddf398bb6f77cfd99450f0376615968e094
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-18 17:48:07 -0600

    Terminando los servicios del curso

--- src/index.js
+++ src/index.js
@@ -7,8 +7,14 @@ const app = express();
 
 app.use(express.json());
 
+// Rutas del servicio
 app.use(indexRoutes);
 app.use("/api", cursosRoutes);
+
+// Ruta no encontrada
+app.use((req, res, next) => {
+    res.status(404).json({ "mensaje": "No se encontro el endpoint" });
+})
 
 app.listen(3000);
 console.log("Servidor ejecutandose en el puerto 3000");
