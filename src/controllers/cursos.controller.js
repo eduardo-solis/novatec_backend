229f6762aed739d988c4736e00ffe215e5c00fdd
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-18 17:34:41 -0600

    Agregando el manejo de errores para los servicios del curso

--- src/controllers/cursos.controller.js
+++ src/controllers/cursos.controller.js
@@ -2,30 +2,44 @@ import { pool } from "../db.js";
 
 
 export const obtenerCursos = async (req, res) => {
-    const [rows] = await pool.query("SELECT * FROM vista_curso");
-    res.json(rows);
+    try {
+        
+        const [rows] = await pool.query("SELECT * FROM vista_curso");
+        res.json(rows);
+
+    } catch (error) {
+        res.status(500).json({"mensaje": "Algo salio mal"});
+    }
 }
 
 export const obtenerCurso = async (req, res) => {
-    const [rows] = await pool.query("SELECT * FROM vista_curso WHERE Id = ?", [req.params.id]);
+    try {
 
-    if (rows <= 0) return res.status(404).json({ "mensaje": "No se encontro ningun curso" })
+        const [rows] = await pool.query("SELECT * FROM vista_curso WHERE Id = ?", [req.params.id]);
+        if (rows <= 0) return res.status(404).json({ "mensaje": "No se encontro ningun curso" })
+        res.send(rows[0]);
 
-    res.send(rows[0]);
+    } catch (error) {
+        res.status(500).json({"mensaje": "Algo salio mal"});
+    }
 }
 
 export const crearCurso = async (req, res) => {
 
-    const { nombre, descripcion, precio } = req.body;
+    try {
 
-    const [rows] = await pool.query(`INSERT INTO curso (nombre, descripcion, precio) value (?,?,?)`, [nombre, descripcion, precio]);
+        const { nombre, descripcion, precio } = req.body;
+        const [rows] = await pool.query(`INSERT INTO curso (nombre, descripcion, precio) value (?,?,?)`, [nombre, descripcion, precio]);
+        res.send({
+            Id: rows.insertId,
+            Nombre: nombre,
+            Descripcion: descripcion,
+            Precio: precio
+        });
 
-    res.send({
-        Id: rows.insertId,
-        Nombre: nombre,
-        Descripcion: descripcion,
-        Precio: precio
-    });
+    } catch (error) {
+        res.status(500).json({"mensaje": "Algo salio mal"});
+    }
 
 }
 
@@ -34,32 +48,37 @@ export const modificarCurso = async (req, res) => {
     const { id } = req.params;
     const { nombre, descripcion, precio } = req.body;
 
-    const [result] = await pool.query("UPDATE curso SET nombre = IFNULL(?, nombre), descripcion = IFNULL(?, descripcion), precio = IFNULL(?, precio) WHERE idCurso = ?", [nombre, descripcion, precio, id]);
+    try {
+        
+        const [result] = await pool.query("UPDATE curso SET nombre = IFNULL(?, nombre), descripcion = IFNULL(?, descripcion), precio = IFNULL(?, precio) WHERE idCurso = ?", [nombre, descripcion, precio, id]);
+        if( result.affectedRows <= 0 ) return res.status(404).json({ "mensaje" : "No se encontro ningun curso" });
+        const [rows] = await pool.query("SELECT * FROM vista_curso WHERE Id = ?", [id]);
+        res.send(rows[0]);
 
-    if( result.affectedRows <= 0 ) return res.status(404).json({ "mensaje" : "No se encontro ningun curso" });
-
-    const [rows] = await pool.query("SELECT * FROM vista_curso WHERE Id = ?", [id]);
-
-    res.send(rows[0]);
-
+    } catch (error) {
+        res.status(500).json({"mensaje": "Algo salio mal"});
+    }
 }
 
 export const cambiarEstatusCurso = async (req, res) => {
     const { id, op } = req.params;
     let sql = "select 1 + 1";
 
-    if( op == 0 ){ // Eliminando curso
-        sql = "UPDATE curso SET estatus = false WHERE idCurso = ?";
+    try {
+        
+        if( op == 0 ){ // Eliminando curso
+            sql = "UPDATE curso SET estatus = false WHERE idCurso = ?";
+        }
+        if ( op == 1 ){ // Activando curso
+            sql = "UPDATE curso SET estatus = true WHERE idCurso = ?";
+        }
+
+        const [result] = await pool.query(sql, [id]);
+        if( result.affectedRows <= 0 ) return res.status(404).json({ "mensaje" : "No se encontro ningun curso" });
+        res.sendStatus(204);
+
+    } catch (error) {
+        res.status(500).json({"mensaje": "Algo salio mal"});
     }
-
-    if ( op == 1 ){ // Activando curso
-        sql = "UPDATE curso SET estatus = true WHERE idCurso = ?";
-    }
-
-    const [result] = await pool.query(sql, [id]);
-
-    if( result.affectedRows <= 0 ) return res.status(404).json({ "mensaje" : "No se encontro ningun curso" });
-
-    res.sendStatus(204);
 
 }
