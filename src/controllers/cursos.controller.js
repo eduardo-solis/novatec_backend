16ae5560f13f160f2343f4b002cc969a6a7533e0
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-18 16:05:52 -0600

    Creando la lectura de los cursos

--- src/controllers/cursos.controller.js
+++ src/controllers/cursos.controller.js
@@ -1,12 +1,21 @@
 import { pool } from "../db.js";
 
 
-export const obtenerCursos = (req, res) => {
-    res.send("obteniendo cursos");
+export const obtenerCursos = async (req, res) => {
+    const [rows] = await pool.query("SELECT * FROM vista_curso");
+    res.json(rows);
+}
+
+export const obtenerCurso = async (req, res) => {
+    const [rows] = await pool.query("SELECT * FROM vista_curso WHERE Id = ?", [req.params.id]);
+
+    if (rows <= 0) return res.status(404).json({ "mensaje": "No se encontro ningun curso" })
+
+    res.send(rows[0]);
 }
 
 export const crearCurso = async (req, res) => {
-    
+
     const { nombre, descripcion, precio } = req.body;
 
     const [result] = await pool.query(`SELECT agregar_curso(?,?,?) AS insertId`, [nombre, descripcion, precio]);
--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { obtenerCursos ,crearCurso, modificarCurso, cambiarEstatusCurso } from "../controllers/cursos.controller.js";
+import { obtenerCursos, obtenerCurso ,crearCurso, modificarCurso, cambiarEstatusCurso } from "../controllers/cursos.controller.js";
 
 const router = Router();
 
 router.get("/curso", obtenerCursos)
+
+router.get("/curso/:id", obtenerCurso)
 
 router.post("/curso", crearCurso)
 
