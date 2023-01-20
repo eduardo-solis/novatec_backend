f0f69a9cc5c435db999e4c895fbc5ae4d9a31193
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-19 20:08:59 -0600

    Agregando las rutas para el servicio de los archivos

---
+++ src/routes/archivos.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get ("/archivo", (req, res) => {
+    res.send("obteniendo archivos");
+});
+
+router.get ("/archivo/:id", (req, res) => {
+    res.send("obteniendo 1 archivo");
+});
+
+router.post ("/archivo", (req, res) => {
+    res.send("agregando archivo");
+});
+
+router.patch ("/archivo", (req, res) => {
+    res.send("modificando archivo");
+});
+
+router.delete ("/archivo/:id/:op", (req, res) => {
+    res.send("cambiando estado archivo");
+});
+
+router.delete ("/archivo/:id", (req, res) => {
+    res.send("borrar archivo");
+});
+
+export default router;
