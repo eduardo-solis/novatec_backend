1364d1f35e160bc9649d0108648a371da27d5d1d
eduardo-solis <solishernandezeduardosolis@gmail.com>
2023-01-24 20:51:04 -0600

    Agregando los servicios del cuestionario

---
+++ src/routes/cuestionarios.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+    obtenerCuestionarios,
+    obtenerCuestionario,
+    crearCuestionario,
+    modificarCuestionario,
+    eliminarCuestionario
+} from '../controllers/cuestionarios.controller.js';
+
+const router = Router();
+
+router.get("/cuestionario", obtenerCuestionarios);
+router.get("/cuestionario/:id", obtenerCuestionario);
+router.post("/cuestionario", crearCuestionario);
+router.patch("/cuestionario/:id", modificarCuestionario);
+router.delete("/cuestionario", eliminarCuestionario);
